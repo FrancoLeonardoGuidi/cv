a42273288c01a377fb359e9c5a2989f77eef6666
Franco Guidi <francoleonardoguidi@gmail.com>
2023-02-28 19:35:57 -0300

    Primera escritura en el repositorio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function mostrarOcultar(id) {
+    var contenido = document.getElementById(id);
+    if (contenido.style.display === "none") {
+        contenido.style.display = "block";
+    } else {
+        contenido.style.display = "none";
+    }
+}
