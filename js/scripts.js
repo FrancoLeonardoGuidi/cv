39d8286bb475345cd27fa9cb7f484d80a7804e1c
Franco Guidi <francoleonardoguidi@gmail.com>
2023-03-09 14:06:50 -0300

    Imágenes cambiadas, funciones agregadas, revisión general de código

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 function mostrarOcultar(id) {
-    var contenido = document.getElementById(id);
+    let contenido = document.getElementById(id);
     if (contenido.style.display === "none") {
         contenido.style.display = "block";
     } else {
         contenido.style.display = "none";
+    }
+}
+
+function reproducirAudio() {
+    let audio = document.getElementById("parlante");
+    let image = document.getElementsByTagName("img")[0];
+
+    if (audio.paused) {
+        audio.play();
+        image.src = "media/images/volume-high-solid.svg";
+    } else {
+        audio.pause();
+        image.src = "media/images/volume-xmark-solid.svg";
     }
 }
